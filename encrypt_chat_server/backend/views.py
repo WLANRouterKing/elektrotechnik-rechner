31d8e607587cd172a221161d3412105119b45b84
LXKennstenich <contact@alexweese.de>
2019-10-23 08:17:02 +0200

    projekt struktur angepasst

--- encrypt_chat_server/backend/views.py
+++ encrypt_chat_server/backend/views.py
@@ -1,11 +1,13 @@
 from flask import render_template, request, flash
-
+from encrypt_chat_server.models import Encryption
 from . import backend
 from .forms import LoginForm
 
 
 @backend.route("/login", methods=["GET", "POST"])
 def login():
+    encryption = Encryption()
+    print(encryption.get_sym_key())
     form = LoginForm()
     if request.method == "POST":
         flash("Login nicht erfolgreich")
---
+++ encrypt_chat_server/models.py
@@ -0,0 +1,286 @@
+import os
+import datetime
+import mysql.connector
+from flask import jsonify
+import nacl.utils
+import nacl.secret
+import nacl.encoding
+import nacl.pwhash
+from nacl import encoding
+from nacl.public import PrivateKey, Box
+
+
+class System:
+
+    def write_file(self, path, content, overwrite):
+        if not overwrite:
+            f = open(path, "a+", encoding='utf-8')
+        else:
+            f = open(path, "wb")
+        f.write(content)
+        f.close()
+
+    def read_file(self, path):
+        f = open(path, "rb")
+        content = f.read()
+        f.close()
+        return content
+
+    def file_exists(self, path):
+        return os.path.isfile(path)
+
+    def log(self, data):
+        message = datetime.datetime.now().strftime("d.m.Y H:i:s")
+        message += " " + data + "\n"
+        self.write_file("debug.log", message, False)
+
+    def log_exception(self, data):
+        message = "Ausnahme: " + format(data)
+        self.log(message)
+
+
+class Database:
+    """
+    init funktion setzt tabellen namen, system objekt, arrData und initialisiert die datenbankverbindung
+    """
+
+    def __init__(self):
+        self.system = System()
+        self.table_name = ""
+        self.arrData = dict()
+
+    """
+    generische get funktion -> muss zur sicherheit noch ergänzt werden
+    """
+
+    def get(self, key):
+        if key in self.arrData:
+            return self.arrData[key]
+        else:
+            return 0
+
+    """
+    generische set funktion -> muss zur sicherheit noch ergänzt werden
+    """
+
+    def set(self, key, value):
+        self.arrData[key] = value
+
+    def get_id(self):
+        return self.get("id")
+
+    def get_user_id(self):
+        return self.get("user_id")
+
+    """
+    gibt ein MySQLConnection Objekt zurück
+    """
+
+    def get_connection(self):
+        return mysql.connector.connect(host="localhost", database="encrypt_chat", user="encrypt_chat",
+                                       password="root")
+
+    """
+    lädt einen datensatz anhand der id oder user_id aus der datenbank
+    ist keine id angegeben wird die komplette tabelle geladen
+    """
+
+    def load(self):
+        success = False
+        connection = self.get_connection()
+        cursor = connection.cursor()
+        table = self.table_name
+
+        try:
+            data = self.arrData
+            values = list(data.values())
+            if int(self.get("id")) > 0:
+                sql = """SELECT * FROM {0} WHERE id = %s""".format(table)
+                cursor = connection.cursor(prepared=True)
+                cursor.execute(sql, values)
+            else:
+                sql = """SELECT * FROM {0}""".format(table)
+                cursor.execute(sql)
+
+            result = []
+            columns = tuple([str(d[0]) for d in cursor.description])
+            for row in cursor:
+                for v in row:
+                    print(v)
+                    try:
+                        v.decode("utf-8")
+                    except:
+                        pass
+                result.append(dict(zip(columns, row)))
+            self.arrData = result
+            success = True
+        except mysql.connector.Error as error:
+            self.system.log_exception("Loading failed ({0}) for following reason: {1}".format(table, error))
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+        return success
+
+    """
+    generische update funktion
+    verarbeitet die key,value pairs aus arrData in ein prepared statement mit platzhaltern
+    und führt danach die query aus
+    """
+
+    def update(self):
+        table = self.table_name
+        id = self.get("id")
+        data = self.arrData
+        for d in data:
+            try:
+                d.decode("utf-8")
+            except:
+                str(d)
+                pass
+        update_string = ""
+        columns = data.keys()
+
+        max_keys = len(data.keys())
+
+        i = 1
+        for column in columns:
+            update_string += column
+            update_string += " = "
+            update_string += " %s"
+            if i < max_keys:
+                update_string += ", "
+            i += 1
+
+        data["last_id"] = id
+        values = list(data.values())
+        sql = """UPDATE {0} SET {1} WHERE id = %s """.format(table, update_string)
+
+        connection = self.get_connection()
+        cursor = connection.cursor(prepared=True)
+        print(sql)
+        print(values)
+        success = cursor.execute(sql, values)
+
+        return str(success)
+
+    """
+    generische insert methode
+    verarbeitet die key,value pairs aus arrData in ein prepared statement mit platzhaltern
+    und führt danach die query aus
+    """
+
+    def insert(self):
+        try:
+            table = self.table_name
+            data = self.arrData
+            columns = ','.join(data.keys())
+            placeholders = ','.join(['%s'] * len(data))
+            values = list(data.values())
+            sql = """INSERT INTO {0} ({1}) VALUES ({2});""".format(table, columns, placeholders)
+            connection = self.get_connection()
+            cursor = connection.cursor(prepared=True)
+            cursor.execute(sql, values)
+            success = cursor.rowcount
+            connection.commit()
+            cursor.close()
+            connection.close()
+        except Exception as error:
+            return "Ausnahme: ".format(error)
+        return str(success)
+
+    """
+    generische speicher methode anhand der id wird überprüft ob der datensatz aus der Datenbank
+    geladen wurde. Wenn dem so ist sollte die id > 0 sein und somit wird ein update() ausgeführt
+    andernfalls wird insert() aufgerufen
+    """
+
+    def save(self):
+        if self.get("id") > 0:
+            return self.update()
+        else:
+            return self.insert()
+
+    def get_json_response(self):
+        return jsonify(self.arrData)
+
+
+class KeyPair(Database):
+
+    def __init__(self):
+        super().__init__()
+        self.table_name = "keypair"
+
+    def get_private_key(self):
+        return self.get("private_key")
+
+    def get_public_key(self):
+        return self.get("public_key")
+
+    def set_private_key(self, value):
+        self.set("private_key", value)
+
+    def set_public_key(self, value):
+        self.set("public_key", value)
+
+
+class Encryption:
+
+    def __init__(self):
+        self.system = System()
+
+    def create_sym_key(self):
+        return nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+
+    """
+    liefert den schlüssel zum symmetrischen verschlüsseln
+    """
+
+    def get_sym_key(self):
+        return "test"
+
+    """
+    symmetrische verschlüsselungsmethode
+    """
+
+    def encrypt(self, data):
+        try:
+            box = nacl.secret.SecretBox(self.get_sym_key())
+            return nacl.encoding.Base64Encoder.encode(box.encrypt(data))
+        except Exception as error:
+            self.system.log_exception(error)
+            return False
+
+    """
+    symmetrische entschlüsselungsmethode
+    """
+
+    def decrypt(self, data):
+        try:
+            box = nacl.secret.SecretBox(self.get_sym_key())
+            return box.decrypt(nacl.encoding.Base64Encoder.decode(data))
+        except Exception as error:
+            self.system.log_exception(error)
+            return False
+
+    """
+    liefert einen password hash zurück (argon2id)
+    """
+
+    def hash_password(self, password):
+        return nacl.pwhash.argon2id.str(password.encode())
+
+    def as_base_64(self, data):
+        return nacl.encoding.Base64Encoder.encode(data)
+
+    def get_key_pair(self):
+        secret_key = PrivateKey.generate()
+        public_key = secret_key.public_key
+        public_key_encrypted = self.encrypt(public_key.encode(encoder=encoding.RawEncoder))
+        private_key_encrypted = self.encrypt(secret_key.encode(encoder=encoding.RawEncoder))
+        key_pair = KeyPair()
+        key_pair.set_private_key(private_key_encrypted)
+        key_pair.set_public_key(public_key_encrypted)
+        return key_pair
