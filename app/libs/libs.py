38b7473cf8e043b7f68dacde8a342da6742cd753
LXKennstenich <contact@alexweese.de>
2019-12-04 19:54:21 +0100

    kleine korrekturen

--- app/libs/libs.py
+++ app/libs/libs.py
@@ -5,6 +5,7 @@ from translations import COLUMN_LABELS
 
 def translate_column(key):
     # language = current_user.language
+    #
     language = "DE"
     if key in COLUMN_LABELS:
         return COLUMN_LABELS[key][language]
